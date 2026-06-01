3c8b1fe448e74204febac604d5b73b980fbaae8a
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-06-01 17:11:39 +0300

    ....

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ShippingInfo from './pages/ShippingInfo';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="app-structural-container">
+        {/* Sticky Global Top Header Navigation */}
+        <Navbar />
+        
+        {/* Isolated Viewport Core Content Context */}
+        <main className="main-viewport-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/shipping-info" element={<ShippingInfo />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />} />
+          </Routes>
+        </main>
+        
+        {/* Informational Desktop/Mobile Grid Footer */}
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
